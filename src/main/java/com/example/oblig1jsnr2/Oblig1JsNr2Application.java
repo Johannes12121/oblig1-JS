ee64bb5e110b948cdadd5be4dde1d49363027c38
Johannes12121 <josae4857@oslomet.no>
2024-02-12 18:44:11 +0100

    første(:

---
+++ src/main/java/com/example/oblig1jsnr2/Oblig1JsNr2Application.java
@@ -0,0 +1,13 @@
+package com.example.oblig1jsnr2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig1JsNr2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig1JsNr2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oblig1jsnr2/Oblig1JsNr2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig1jsnr2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig1JsNr2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
